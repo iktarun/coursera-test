72c7c455526ffca3fafc9fb712e31c61f9dd96e1
tarunkumar2215 <tarunkumar2215>
2017-04-14 12:50:24 +0530

    Name calculator application

---
+++ app.js
@@ -0,0 +1,37 @@
+// (function(){
+// 	'use strict';
+// 	angular.module("myFirstApp", [])
+// 	.controller('MyFirstController', function($scope){
+// 		$scope.name = "Tarun";
+// 		$scope.sayHello =  function(){
+// 			return "Hello Coursera";
+// 		};
+
+// 	});
+// })();
+
+// ************************ Name Calculator App **************** //
+(function(){
+	'use-strict';
+	angular.module("NameCalculatorApp",[]).
+		controller('NameCalculatorController', function($scope){
+		
+		$scope.name 		= "";
+		$scope.totalValue 	= 0;
+
+		$scope.displayNumeric = function() {
+			var totalNameValue = calculateNumericForString($scope.name);
+			$scope.totalValue  = totalNameValue; 
+		};
+
+		function calculateNumericForString(Mystring){
+			var totalStringValue = "";
+			for(var i=0; i< Mystring.length; i++){
+				totalStringValue += Mystring.charCodeAt(i);
+			}
+			return totalStringValue;
+		}
+	});
+})();
+
+//**************************************************** //
